24640ee76dd1bed1306ccd429503934138f0ec2e
John Lambert <john_lambert@sil.org>
2023-05-15 15:56:54 -0400

    At request of Damien, remove saving the model option - Moved to #12

--- samples/clearml_run_test.py
+++ samples/clearml_run_test.py
@@ -1,11 +1,10 @@
 import machine.webapi.clearml_nmt_engine_build_job as cml
 
 args = {
-    "engine_id": "existingengineid",
-    "build_id": "existingbuildid",
+    "engine_id": "645d317a6d7b74cbf8d54239",
+    "build_id": "645d317b6d7b74cbf8d54244",
     "src_lang": "es",
     "trg_lang": "en",
     "max_step": 1000,
-    "save_model": False,
 }
 cml.run(args)
