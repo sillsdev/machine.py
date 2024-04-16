5d05e6d7124b4ade79ffaa7e827ce75bed31a25e
mshannon-sil <131058912+mshannon-sil@users.noreply.github.com>
2024-04-16 07:59:44 -0400

    fix normalization order in find_missing_characters() (#105)
    * fix normalization order in find_missing_characters()
    
    * don't check isinstance during iterations

--- machine/translation/huggingface/hugging_face_nmt_model_trainer.py
+++ machine/translation/huggingface/hugging_face_nmt_model_trainer.py
@@ -172,12 +172,14 @@ class HuggingFaceNmtModelTrainer(Trainer):
         def find_missing_characters(tokenizer: Any, train_dataset: Dataset, lang_codes: List[str]) -> List[str]:
             vocab = tokenizer.get_vocab().keys()
             charset = set()
-            for lang_code in lang_codes:
-                for ex in train_dataset["translation"]:
-                    charset = charset | set(ex[lang_code])
-            if isinstance(tokenizer, (NllbTokenizerFast)):
-                charset = {self._mpn.normalize(char) for char in charset}
-            charset = {tokenizer.backend_tokenizer.normalizer.normalize_str(char) for char in charset}
+            mpn_normalize = True if isinstance(tokenizer, (NllbTokenizerFast)) else False
+            for ex in train_dataset["translation"]:
+                for lang_code in lang_codes:
+                    ex_text = ex[lang_code]
+                    if mpn_normalize:
+                        ex_text = self._mpn.normalize(ex_text)
+                    ex_text = tokenizer.backend_tokenizer.normalizer.normalize_str(ex_text)
+                    charset = charset | set(ex_text)
             charset = set(filter(None, {char.strip() for char in charset}))
             missing_characters = sorted(list(charset - vocab))
             return missing_characters
