47f269a663e70d55e4e3b9a7885490768d81af7d
John Lambert <john_lambert@sil.org>
2024-08-23 15:58:38 -0400

    Fixed circular import

--- machine/tokenization/tokenizer_factory.py
+++ machine/tokenization/tokenizer_factory.py
@@ -1,19 +1,17 @@
 from enum import Enum, auto
 from typing import Union
 
-from . import (
-    Detokenizer,
-    LatinSentenceTokenizer,
-    LatinWordDetokenizer,
-    LatinWordTokenizer,
-    LineSegmentTokenizer,
-    NullTokenizer,
-    Tokenizer,
-    WhitespaceDetokenizer,
-    WhitespaceTokenizer,
-    ZwspWordDetokenizer,
-    ZwspWordTokenizer,
-)
+from .detokenizer import Detokenizer
+from .latin_sentence_tokenizer import LatinSentenceTokenizer
+from .latin_word_detokenizer import LatinWordDetokenizer
+from .latin_word_tokenizer import LatinWordTokenizer
+from .line_segment_tokenizer import LineSegmentTokenizer
+from .null_tokenizer import NullTokenizer
+from .tokenizer import Tokenizer
+from .whitespace_detokenizer import WhitespaceDetokenizer
+from .whitespace_tokenizer import WhitespaceTokenizer
+from .zwsp_word_detokenizer import ZwspWordDetokenizer
+from .zwsp_word_tokenizer import ZwspWordTokenizer
 
 
 class TokenizerType(Enum):
