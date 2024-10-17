7f01080df3ddd23b3cd1257ff6670e3a53660ed0
TaperChipmunk32 <matthewbeech737@gmail.com>
2024-10-17 14:05:49 -0500

    Fixed Black formatting

--- machine/translation/huggingface/hugging_face_nmt_model_trainer.py
+++ machine/translation/huggingface/hugging_face_nmt_model_trainer.py
@@ -239,7 +239,7 @@ class HuggingFaceNmtModelTrainer(Trainer):
             )
             lang_id = tokenizer.convert_tokens_to_ids(lang_code)
             tokenizer.lang_code_to_id[lang_code] = lang_id
-            
+
             if isinstance(tokenizer, (NllbTokenizer, MBart50Tokenizer, MBartTokenizer)):
                 tokenizer.id_to_lang_code[lang_id] = lang_code
                 tokenizer.fairseq_tokens_to_ids[lang_code] = lang_id
