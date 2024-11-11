8679b785cdecb2a427726baf55b023a80496498e
mshannon-sil <matthew_shannon@sil.org>
2024-11-11 18:27:31 -0500

    raise error when id tag doesn't match filename book id

--- tests/corpora/test_paratext_backup_text_corpus.py
+++ tests/corpora/test_paratext_backup_text_corpus.py
@@ -2,9 +2,14 @@ from __future__ import annotations
 
 from pathlib import Path
 from tempfile import TemporaryDirectory
-from typing import Any, ContextManager
+from typing import Any, ContextManager, Optional
 
-from testutils.corpora_test_helpers import create_test_paratext_backup
+from pytest import raises
+from testutils.corpora_test_helpers import (
+    create_test_paratext_backup,
+    create_test_paratext_backup_invalid_id,
+    create_test_paratext_backup_mismatch_id,
+)
 
 from machine.corpora import ParatextBackupTextCorpus
 
@@ -28,10 +33,27 @@ def test_get_text() -> None:
         assert not any(jhn.get_rows())
 
 
+def test_invalid_id() -> None:
+    with raises(ValueError, match=r"The \\id tag in .* is invalid."):
+        with _TestEnvironment("invalid_id") as env:
+            env.corpus.get_text("JDG")
+
+
+def test_mismatch_id() -> None:
+    with raises(ValueError, match=r"The \\id tag .* in .* does not match filename book id .*"):
+        with _TestEnvironment("mismatch_id") as env:
+            env.corpus.get_text("JDG")
+
+
 class _TestEnvironment(ContextManager["_TestEnvironment"]):
-    def __init__(self) -> None:
+    def __init__(self, project_folder_name: Optional[str] = None) -> None:
         self._temp_dir = TemporaryDirectory()
-        archive_filename = create_test_paratext_backup(Path(self._temp_dir.name))
+        if project_folder_name == "invalid_id":
+            archive_filename = create_test_paratext_backup_invalid_id(Path(self._temp_dir.name))
+        elif project_folder_name == "mismatch_id":
+            archive_filename = create_test_paratext_backup_mismatch_id(Path(self._temp_dir.name))
+        else:
+            archive_filename = create_test_paratext_backup(Path(self._temp_dir.name))
         self._corpus = ParatextBackupTextCorpus(archive_filename)
 
     @property
