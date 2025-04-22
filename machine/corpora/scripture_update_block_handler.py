6297d8620a0a0b355408bebbb90684c26d124723
John Lambert <john_lambert@sil.org>
2025-04-22 16:57:33 -0400

    Reviewer updates

--- machine/corpora/scripture_update_block_handler.py
+++ machine/corpora/scripture_update_block_handler.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
-from abc import ABC
+from abc import ABC, abstractmethod
 
 from .scripture_update_block import ScriptureUpdateBlock
 
 
 class ScriptureUpdateBlockHandler(ABC):
 
+    @abstractmethod
     def process_block(self, block: ScriptureUpdateBlock) -> ScriptureUpdateBlock: ...
