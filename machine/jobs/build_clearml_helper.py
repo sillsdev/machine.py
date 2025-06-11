047b01d6571bbb21bf0721fbc840c5d7bb5bf7dd
Peter Chapman <peter@conglomo.co.nz>
2025-06-11 12:30:04 +1200

    Report the phase in the runtime

--- machine/jobs/build_clearml_helper.py
+++ machine/jobs/build_clearml_helper.py
@@ -51,31 +51,15 @@ def get_clearml_progress_caller(
                 progress_info.last_progress_time is None
                 or (current_time - progress_info.last_progress_time).seconds > 1
             ):
-                new_runtime_props = task.data.runtime.copy() or {}  # type: ignore
-                new_runtime_props["progress"] = str(percent_completed)
-                new_runtime_props["message"] = message
                 scheduler.schedule(
                     update_runtime_properties(
-                        task.id,  # type: ignore
-                        task.session.host,
-                        task.session.token,  # type: ignore
-                        create_runtime_properties(task, percent_completed, message),
+                        task,
+                        create_runtime_properties(task, percent_completed, message, progress_status),
                     )
                 )
                 progress_info.last_progress_time = current_time
             progress_info.last_percent_completed = percent_completed
             progress_info.last_message = message
-            # Report the step within the phase
-            if isinstance(progress_status, PhaseProgressStatus) and task is not None:
-                if progress_status.phase_stage is not None:
-                    if progress_status.phase_step is not None:
-                        task.get_logger().report_single_value(
-                            name=f"{progress_status.phase_stage}_step", value=progress_status.phase_step
-                        )
-                    if progress_status.step_count is not None:
-                        task.get_logger().report_single_value(
-                            name=f"{progress_status.phase_stage}_step_count", value=progress_status.step_count
-                        )
 
     return clearml_progress
 
@@ -109,21 +93,30 @@ def update_settings(settings: Settings, args: dict):
     settings.data_dir = os.path.expanduser(cast(str, settings.data_dir))
 
 
-async def update_runtime_properties(task_id: str, base_url: str, token: str, runtime_props: dict) -> None:
-    async with aiohttp.ClientSession(base_url=base_url, headers={"Authorization": f"Bearer {token}"}) as session:
-        json = {"task": task_id, "runtime": runtime_props, "force": True}
+async def update_runtime_properties(task, runtime_props: dict) -> None:
+    current_runtime_properties = task.data.runtime or {}
+    current_runtime_properties.update(runtime_props)
+    async with aiohttp.ClientSession(
+        base_url=task.session.host, headers={"Authorization": f"Bearer {task.session.token}"}
+    ) as session:
+        json = {"task": task.id, "runtime": runtime_props, "force": True}
         async with session.post("/tasks.edit", json=json) as response:
             response.raise_for_status()
 
 
-def create_runtime_properties(task, percent_completed: Optional[int], message: Optional[str]) -> dict:
+def create_runtime_properties(
+    task, percent_completed: Optional[int], message: Optional[str], status: Optional[ProgressStatus]
+) -> dict:
     runtime_props = task.data.runtime.copy() or {}
     if percent_completed is not None:
         runtime_props["progress"] = str(percent_completed)
-    else:
-        del runtime_props["progress"]
     if message is not None:
         runtime_props["message"] = message
-    else:
-        del runtime_props["message"]
+    # Report the step within the phase
+    if status is not None and isinstance(status, PhaseProgressStatus):
+        if status.phase_stage is not None:
+            if status.phase_step is not None:
+                runtime_props[f"{status.phase_stage}_step"] = str(status.phase_step)
+            if status.step_count is not None:
+                runtime_props[f"{status.phase_stage}_step_count"] = str(status.step_count)
     return runtime_props
