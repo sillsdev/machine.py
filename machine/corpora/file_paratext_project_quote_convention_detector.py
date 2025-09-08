2afbd4ec36fc8a3791bef5d6996b89576aaa8502
Ben King <benjamin_king@sil.org>
2025-09-08 10:47:14 -0400

    Detect quote convention from file Paratext project (#224)
    * Detect quote convention from file Paratext project
    
    * Address reviewer comments
    
    ---------
    
    Co-authored-by: Ben King <benjaminking@sil.org>

---
+++ machine/corpora/file_paratext_project_quote_convention_detector.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from typing import BinaryIO
+
+from ..utils.typeshed import StrPath
+from .file_paratext_project_settings_parser import FileParatextProjectSettingsParser
+from .paratext_project_quote_convention_detector import ParatextProjectQuoteConventionDetector
+
+
+class FileParatextProjectQuoteConventionDetector(ParatextProjectQuoteConventionDetector):
+    def __init__(self, project_dir: StrPath) -> None:
+        super().__init__(FileParatextProjectSettingsParser(project_dir))
+
+        self._project_dir = project_dir
+
+    def _exists(self, file_name: str) -> bool:
+        return (Path(self._project_dir) / file_name).exists()
+
+    def _open(self, file_name: StrPath) -> BinaryIO:
+        return open(Path(self._project_dir) / file_name, mode="rb")
