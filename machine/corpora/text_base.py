7de8cbaff5f2eb8aaa44413da6e9569e86612e73
Enkidu93 <lowryec17@gcc.edu>
2026-01-22 14:41:34 -0500

    Rename text_base content_type to default_content_type

--- machine/corpora/text_base.py
+++ machine/corpora/text_base.py
@@ -9,7 +9,7 @@ class TextBase(Text):
     def __init__(self, id: str, sort_key: str, content_type: TextRowContentType = TextRowContentType.SEGMENT) -> None:
         self._id = id
         self._sort_key = sort_key
-        self._content_type = content_type
+        self._default_content_type = content_type
 
     @property
     def id(self) -> str:
@@ -20,12 +20,12 @@ class TextBase(Text):
         return self._sort_key
 
     @property
-    def content_type(self) -> TextRowContentType:
-        return self._content_type
+    def default_content_type(self) -> TextRowContentType:
+        return self._default_content_type
 
     def _create_row(self, text: str, ref: Any, flags: TextRowFlags = TextRowFlags.SENTENCE_START) -> TextRow:
         text = text.strip()
-        return TextRow(self.id, ref, [text] if len(text) > 0 else [], flags, content_type=self.content_type)
+        return TextRow(self.id, ref, [text] if len(text) > 0 else [], flags, content_type=self.default_content_type)
 
     def _create_empty_row(self, ref: Any, flags: TextRowFlags = TextRowFlags.NONE) -> TextRow:
-        return TextRow(self.id, ref, flags=flags, content_type=self.content_type)
+        return TextRow(self.id, ref, flags=flags, content_type=self.default_content_type)
