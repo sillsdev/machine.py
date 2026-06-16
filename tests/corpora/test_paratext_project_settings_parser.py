96da6779fc330e61e1502357861f2cd276a27f39
Matthew Beech <matthewbeech737@gmail.com>
2026-06-16 14:22:04 -0500

    Fix formatting

--- tests/corpora/test_paratext_project_settings_parser.py
+++ tests/corpora/test_paratext_project_settings_parser.py
@@ -24,6 +24,7 @@ def test_translation_info_specified() -> None:
     assert settings.parent_name == "DEF"
     assert settings.parent_guid == "22222222222222222222222222222222"
 
+
 def test_normalization_form_default() -> None:
     settings = _create_settings()
 
